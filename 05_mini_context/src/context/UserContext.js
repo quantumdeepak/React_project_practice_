58421b88834f16fa2ea30515fdeeaf2877975b73
quantumdeepak <130047577+quantumdeepak@users.noreply.github.com>
2024-01-20 05:30:44 +0000

    new commit

---
+++ 05_mini_context/src/context/UserContext.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+let UserContext = React.createContext()
+
+export default UserContext;
+
+//  with the contet we need to make an provider 
+//  will add the freagments in usercontext tag i.e. the variable just we created
+
